b7284f494037632c954bf5dd10b8a4a18362808a
vince0519 <93874515+vince0519@users.noreply.github.com>
2025-03-25 16:15:32 -0400

    Time: 2 ms (46.56%) Space: 12.5 MB (46.11%)

---
+++ 0392-is-subsequence/is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+
+        s_pointer = 0
+
+        for c in t:
+            if c == s[s_pointer]:
+                s_pointer += 1
+
+            if s_pointer == len(s):
+                return True
+
+        return False
