a6cbacb42c46fbfda32ae85fcbc83584285e26bd
vince0519 <93874515+vince0519@users.noreply.github.com>
2025-03-25 16:58:53 -0400

    Time: 165 ms (5.15%) Space: 20.9 MB (14.26%)

--- 0011-container-with-most-water/container-with-most-water.py
+++ 0011-container-with-most-water/container-with-most-water.py
@@ -3,24 +3,18 @@ class Solution(object):
         return length * min(height_l, height_r)
 
     def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        l, r, qty = 0, len(height)-1, 0
+        l, r = 0, len(height) - 1
         qty = 0
 
         while l < r:
             length = r - l
             temp_qty = self.calculateQuantity(height[l], height[r], length)
-            if (temp_qty >= qty):
-                qty = temp_qty
+            qty = max(qty, temp_qty)  # More concise than the if block
 
-            if (height[l] < height[r]):
+            # Move the shorter wall inward
+            if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        
-        return qty
 
-        
+        return qty
